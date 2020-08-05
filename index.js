281f2d0c3ed544cf07468118aa28b71311a9ac2b
Rakesh Haridas <rakeshharidas@Rakeshs-MacBook-Air.local>
2020-08-06 02:30:04 +0530

    express demo

---
+++ index.js
@@ -0,0 +1,47 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id == parseInt(req.params.id));
+  if (!course)
+    res.status(404).send("The course with the given Id was not found!");
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  const validationResult = schema.validate({});
+
+  if (validationResult.error) {
+    res.status(400).send(validationResult.error.details[0].message);
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}`));
